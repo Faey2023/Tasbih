1e14af5ebf5eaf1001cae85751164fae17d717e4
Fahima <sfahima1388@gmail.com>
2023-08-14 20:37:07 +0600

    Added incre decre JS

--- index.js
+++ index.js
@@ -2,16 +2,58 @@
 const subhanAllahDisplay = document.getElementById("subhanAllahDisplay");
 const sIncrement = document.getElementById("s-increment");
 const sDecrement = document.getElementById("s-decrement");
+sIncrement.addEventListener("click", function () {
+  if (sInitialValue >= 33) {
+    return alert("Alhamdulillah! You're done with your tasbih.");
+  }
+  sInitialValue += 1;
+  subhanAllahDisplay.innerText = sInitialValue;
+});
+sDecrement.addEventListener("click", function () {
+  if (sInitialValue == 0) {
+    return alert("You can not go further.");
+  }
+  sInitialValue -= 1;
+  subhanAllahDisplay.innerText = sInitialValue;
+});
 
 // Alhamdulillah
 const AlhamdulillahDisplay = document.getElementById("AlhamdulillahDisplay");
 const aIncrement = document.getElementById("a-increment");
 const aDecrement = document.getElementById("a-decrement");
+aIncrement.addEventListener("click", function () {
+  if (aInitialValue >= 33) {
+    return alert("Alhamdulillah! You're done with your tasbih.");
+  }
+  aInitialValue += 1;
+  AlhamdulillahDisplay.innerText = aInitialValue;
+});
+aDecrement.addEventListener("click", function () {
+  if (aInitialValue == 0) {
+    return alert("You can not go further.");
+  }
+  aInitialValue -= 1;
+  AlhamdulillahDisplay.innerText = aInitialValue;
+});
 
 // Allahu Akbar
 const AllahuakbarDisplay = document.getElementById("AllahuakbarDisplay");
 const akIncrement = document.getElementById("ak-increment");
 const akDecrement = document.getElementById("ak-decrement");
+akIncrement.addEventListener("click", function () {
+  if (akInitialValue >= 33) {
+    return alert("Alhamdulillah! You're done with your tasbih.");
+  }
+  akInitialValue += 1;
+  AllahuakbarDisplay.innerText = akInitialValue;
+});
+akDecrement.addEventListener("click", function () {
+  if (akInitialValue == 0) {
+    return alert("You can not go further.");
+  }
+  akInitialValue -= 1;
+  AllahuakbarDisplay.innerText = akInitialValue;
+});
 
 //reset button
 const buttonReset = document.getElementById("btn-reset");
@@ -19,16 +61,3 @@ const buttonReset = document.getElementById("btn-reset");
 let sInitialValue = 0;
 let aInitialValue = 0;
 let akInitialValue = 0;
-
-sIncrement.addEventListener("click", function () {
-  sInitialValue += 1;
-  subhanAllahDisplay.innerText = sInitialValue;
-});
-sIncrement.addEventListener("click", function () {
-  aInitialValue += 1;
-  AlhamdulillahDisplay.innerText = aInitialValue;
-});
-sIncrement.addEventListener("click", function () {
-  akInitialValue += 1;
-  AllahuakbarDisplay.innerText = akInitialValue;
-});
